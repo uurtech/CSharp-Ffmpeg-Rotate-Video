9f6d29644719233dffc2c1a7e4e63fab2963c560
Ugur KAZDAL <ugur.kazdal@1ki3.com>
2015-12-30 12:51:48 +0200

    basic çalışan hali hazır

---
+++ RotatorForm/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RotatorForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string fileName = string.Empty;
+        OpenFileDialog file = new OpenFileDialog();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            file.ShowDialog();
+            fileName = file.FileName;
+            textBox1.Text = fileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "ffmpeg.exe";
+            process.StartInfo.Arguments = "-i " + fileName + " -vf \"transpose = 1\" " + fileName;
+            process.Start();
+        }
+    }
+}
